b0d70774ecae995faee1787e54ea1995bd6afb41
SandstoneHobo <60045780+SandstoneHobo@users.noreply.github.com>
2024-01-30 11:52:13 -0600

    Made the menu functional and have input handling

--- HomeAssignment1OPSystem.cpp
+++ HomeAssignment1OPSystem.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+
+void generateMenu(){
+    std::cout << std::endl;
+    std::cout << "1. List directory contents" << std::endl;
+    std::cout << "2. Print working directoy" << std::endl;
+    std::cout << "3. Create a new directory" << std::endl;
+    std::cout << "4. Display a message" << std::endl;
+    std::cout << "5. Concatenate and display content of a file" << std::endl;
+    std::cout << "6. Exit" << std::endl;
+    std::cout << std::endl;
+}
+
+void listDirectory() {
+    std::cout << "PlaceHolder";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char input = '0';
+    while (input != '6') {
+        bool valid = false;
+        generateMenu();
+        std::cin >> input;
+        switch (input) {
+            case '1':
+                listDirectory();
+                break;
+            default:
+                std::cout << "You gave an invalid input";
+                break;
+        }
+    }
 }
 
